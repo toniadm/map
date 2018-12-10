52e922f62361a90af0094ffd707a9555be733b55
TDM <toniadm@swbell.net>
2018-12-10 09:46:17 -0600

    Service worker

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 // eslint-disable-next-line
-import serviceWorker from './serviceWorker';
+//import serviceWorker from './serviceWorker';
+import registerServiceWorker from 'react-service-worker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker();
+registerServiceWorker();
